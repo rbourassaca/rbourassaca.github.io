0d8256c785ddeb1ca20a8fc0646436b948eb83f3
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-11-04 22:34:03 -0500

    Modification du component image afin de le rendre réutilisable.

--- src/components/image.js
+++ src/components/image.js
@@ -13,24 +13,12 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
-  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+const Image = ( {imageUrl} ) => {
+  if (!imageUrl) {
     return <div>Picture not found</div>
   }
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img fluid={imageUrl} />
 }
 
 export default Image
