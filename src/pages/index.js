065112feeeaa46e17158a75fa332dd7c3962951b
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-11-04 22:50:15 -0500

    Suppression de style innutile.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <Image url="../images/favicon.png"/>
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
