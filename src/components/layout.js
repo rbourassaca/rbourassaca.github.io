b650b25c804a3e8ebcf619fdaf15110cbd152455
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-11-04 22:07:58 -0500

    Ajout d'un component pour le menu.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
+import Menu from "./menu"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,6 +27,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Menu />
       <div>
         <main>{children}</main>
       </div>
---
+++ src/components/menu.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Menu = () => (
+  <nav>
+    <Link to="/">Menu item 1</Link>
+    <Link to="/">Menu item 2</Link>
+    <Link to="/">Menu item 3</Link>
+  </nav>
+)
+
+export default Menu
