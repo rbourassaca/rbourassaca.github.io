bcc8ae736e1d2c81b81d7bb8bea1777f7bbcd28f
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-11-05 20:40:22 -0500

    Ajout d'un menu bootstrap de base.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,12 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Navbar, Nav } from "react-bootstrap"
 
 const Menu = ({siteTitle}) => (
-  <nav>
-    <Link to="/">Link 1</Link>
-    <Link to="/">Link 2</Link>
-    <Link to="/">Link 3</Link>
-  </nav>
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">
+      Raphael Bourassa
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/random" className="nav-link">404</Link>
+        </li>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 export default Menu
