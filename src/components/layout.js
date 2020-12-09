b72eb412668c35d031dee825486ed18a228a38ee
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-12-08 22:54:37 -0500

    On importe le style.

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import "./layout.scss"
+
 import Header from "./header/header"
 import Footer from "./footer/footer"
 
