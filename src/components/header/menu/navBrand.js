1027dc86679d3ba384b57444faf8cd6a807abd0f
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-12-22 22:57:03 -0500

    Ajout du logo.

--- src/components/header/menu/navBrand.js
+++ src/components/header/menu/navBrand.js
@@ -1,9 +1,25 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-function NavBrand(props){
+
+const NavBrand = (props) => {
+    const logo = useStaticQuery(graphql`
+        query{
+            file(relativePath: { eq: "logo.png" }){
+                childImageSharp{
+                    fixed(width: 55){
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            }
+        }
+    `)
+
     return(
-        <Link to={props.url} className="navbar-brand">{props.title}</Link> 
+        <Link to={props.url} classname="navbar-brand">
+            <Img fixed={logo.file.childImageSharp.fixed}/>
+        </Link>
     )
 }
 
