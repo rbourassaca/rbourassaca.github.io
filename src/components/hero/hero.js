697a8d7ae97fd546b91b79f007b5919fe9bbc8b0
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-01-05 18:13:25 -0500

    Ajout des liens pour les réseaux sociaux.

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,20 +1,70 @@
 import React from "react"
 import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
 import { Container } from "react-bootstrap"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faInstagram, faLinkedin, faYoutube, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import "./hero.scss"
 
 var background;
+var social = [];
+
 
 const Hero = (props) =>{
+    const query = useStaticQuery(graphql `
+        query MyQuery {
+            wp{
+                heroSettings{
+                    reseauxSociaux{
+                        liens{
+                            reseau
+                            lien
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    query.wp.heroSettings.reseauxSociaux.liens.map((item, index) => {
+        switch(item.reseau){
+            case "Facebook":
+                social.push({"link" : item.lien, "icon" : faFacebookF});
+                break;
+            case "Instagram":
+                social.push({"link" : item.lien, "icon" : faInstagram});
+                break;
+            case "LinkedIn":
+                social.push({"link" : item.lien, "icon" : faLinkedin});
+                break;
+            case "Youtube":
+                social.push({"link" : item.lien, "icon" : faYoutube});
+                break;
+            case "GitHub":
+                social.push({"link" : item.lien, "icon" : faGithub});
+                break;
+        }
+    })
+
     if(typeof(props.background) === "object"){
         background = props.background[Math.floor((Math.random() * props.background.length))].node.childImageSharp.fluid
     }
+
     return(
         <section className="hero">
             <Img fluid={background} className="background"/>
+            <aside>
+                <ul>
+                    {social.map((item, index) =>
+                    <li>
+                        <a href={item.link} key={index}>
+                            <FontAwesomeIcon icon={item.icon}/>
+                        </a>
+                    </li>
+                    )}
+                </ul>
+            </aside>
             <Container>
                 {props.logo &&
                     <Img fluid={props.logo} alt="Logo du site web" className="logo"/>
