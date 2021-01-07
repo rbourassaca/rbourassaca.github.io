851c67fb59ded4ba83c970f435eb700b2269745b
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-01-06 22:55:24 -0500

    Correction d'une erreur.

--- src/wp/wp_archive_template.js
+++ src/wp/wp_archive_template.js
@@ -42,7 +42,7 @@ export const query = graphql`
                             }
                         }
                     }
-                    web{
+                    client{
                         localFile{
                             childImageSharp{
                                 fluid(maxWidth: $maxWidth, quality: $quality){
