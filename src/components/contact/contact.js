aab128a1e34d20788d1f45cb2d9ce1971a62cdcc
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-01-21 14:59:44 -0500

    Transformation du formulaire de contact sous bootstrap.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,21 +1,27 @@
 import React from "react"
-import { Container } from "react-bootstrap"
+import { Container, Form, Button } from "react-bootstrap"
 import "./contact.scss"
 
 const Contact = () => {
     return(
         <Container>
-            <form>
-                <label>Votre nom:
-                    <input type="text" name="name" id="name"/>
-                </label>
-                <label>Votre email:
-                    <input type="email" name="email" id="email"/>
-                </label>
-                <label>Votre message:
-                    <textarea name="message" rows="5"/>
-                </label>
-            </form>
+            <Form>
+                <Form.Group controlId="name">
+                    <Form.Label>Nom:</Form.Label>
+                    <Form.Control type="text" required placeholder="Entrer votre nom"/>
+                </Form.Group>
+                <Form.Group controlId="email">
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control type="email" required placeholder="Entrer votre email"/>
+                </Form.Group>
+                <Form.Group controlId="message">
+                    <Form.Label>Message:</Form.Label>
+                    <Form.Control as="textarea" required rows={5} placeholder="Entrer votre message"/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Envoyer
+                </Button>
+            </Form>
         </Container>
     )
 }
