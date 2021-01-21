878310f14f3e561e3495e6ac9267130ec6bf8074
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-01-21 11:37:09 -0500

    Ajout de gatsby-plugin-transition-link.

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,8 +61,9 @@ module.exports = {
         google: {
           families: ['Roboto']
         }
-      }
-    }
+      },
+    },
+    `gatsby-plugin-transition-link`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
