629de3da95f4a4e79b5cf7423c57cee58b73bcc4
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-01-21 12:02:52 -0500

    Suppression de gatsby-plugin-transition-link et gsap.

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,8 +62,7 @@ module.exports = {
           families: ['Roboto']
         }
       },
-    },
-    `gatsby-plugin-transition-link`
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
