3ee71768e95e90a91a71f4f46eb9cd8c96fd4a7d
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-01-21 12:20:06 -0500

    Alternance entre les couleurs des blocs de contenu.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./footer.scss"
 
 const Footer = () => (
   <footer>
-    <div className="container py-4">
+    <div className="container">
       <p>© {new Date().getFullYear()}, Raphael Bourassa. Tous droits réservés.</p>
     </div>
   </footer>
