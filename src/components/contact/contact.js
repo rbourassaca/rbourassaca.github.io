525d7e3feb879296b841d65783373e07d42af916
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-01-26 18:01:04 -0500

    Ajout d'un endpoint à la form.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,18 +5,18 @@ import "./contact.scss"
 const Contact = () => {
     return(
         <Container>
-            <Form>
+            <Form action="https://getform.io/f/42517e42-48ff-4710-9bf8-24ce83ae50bb" method="POST">
                 <Form.Group controlId="name">
                     <Form.Label>Nom:</Form.Label>
-                    <Form.Control type="text" required placeholder="Entrer votre nom"/>
+                    <Form.Control name="name" type="text" required placeholder="Entrer votre nom"/>
                 </Form.Group>
                 <Form.Group controlId="email">
                     <Form.Label>Email:</Form.Label>
-                    <Form.Control type="email" required placeholder="Entrer votre email"/>
+                    <Form.Control name="email" type="email" required placeholder="Entrer votre email"/>
                 </Form.Group>
                 <Form.Group controlId="message">
                     <Form.Label>Message:</Form.Label>
-                    <Form.Control as="textarea" required rows={5} placeholder="Entrer votre message"/>
+                    <Form.Control name="message" as="textarea" required rows={5} placeholder="Entrer votre message"/>
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Envoyer
