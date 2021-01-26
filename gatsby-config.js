127e2c0f5a2ff7d394093c0cb87e6d41283e199e
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-01-26 18:13:09 -0500

    Mise à jour des informations du site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}`})
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio de Raphael Bourassa`,
+    description: `Raphael Bourassa, travailleur autonome dans la région des Laurentides oeuvrant dans le domaine du multimédia dont l’audio, le vidéo et le web.`,
+    author: `Raphael Bourassa`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,11 +19,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Portfolio de Raphael Bourassa`,
+        short_name: `Raphael Bourassa`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#161619`,
+        theme_color: `#222327`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
