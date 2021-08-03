6437eb60625f8b177b43c897bee5a69eb1700819
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-08-03 18:19:16 -0400

    On cache d'une façon temporaire le code "&#8217;".

--- src/components/projectList/project/project.js
+++ src/components/projectList/project/project.js
@@ -12,7 +12,7 @@ const Project = ({ project, index, archive, defaultThumbnail }) => {
                 <Col md={8} className="info">
                     <div>
                         <Link to={project.uri}><h2>{project.title}</h2></Link>
-                        <p>{project["acf_"+archive].description.replace(/<\/?[^>]+(>|$)/g, "").slice(0, 400)+"..."}</p>
+                        <p>{project["acf_"+archive].description.replace(/<\/?[^>]+(>|$)/g, "").replace(/&#8217;/g, "'").slice(0, 400)}{project["acf_"+archive].description.length > 400 && "..."}</p>
                     </div>
                 </Col>
                 <Col>
