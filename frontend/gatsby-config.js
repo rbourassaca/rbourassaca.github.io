7dcd0e637f368dae31a0a2013532a81d381e70ef
Raphael Bourassa <bourassa.raphael@outlook.com>
2021-10-24 12:30:33 -0400

    Ajout de gatsby-source-strapi.

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -29,27 +29,14 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-wordpress`,
+      resolve: `gatsby-source-strapi`,
       options: {
-        url: process.env.API_URL,
-        verbose: true,
-        develop: {
-          hardCacheMediaFiles: true,
-        },
-        debug: {
-          graphql: {
-            writeQueriesToDisk: true,
-          },
-        },
-        type: {
-          Post: {
-            limit:
-              process.env.NODE_ENV === `development`
-                ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                  50
-                : // and we don't actually need more than 5000 in production for this particular site
-                  5000,
-          },
+        apiURL: process.env.API_URL,
+        collectionTypes: ['menus', 'pages', 'projects', 'project-categories'],
+        singleTypes: ['frontend-settings'],
+        loginData: {
+          identifier: process.env.API_IDENTIFIER,
+          password: process.env.API_PASSWORD,
         },
       },
     },
