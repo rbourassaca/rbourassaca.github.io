1e8732e5ca1b802a972ce9671c00cfcf28903228
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-01-09 00:03:38 -0500

    Correction de la méthode utilisé pour créer la boucle.

--- frontend/src/components/hero/arrowDown.js
+++ frontend/src/components/hero/arrowDown.js
@@ -2,18 +2,15 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons"
 import { useSpring, animated, config } from "react-spring"
-import { useState } from "react"
 
 const ArrowDown = () => {
-  const [flip, set] = useState(false)
   const props = useSpring({
+    loop: { reverse: true },
     to: { transform: "translateY(-20px)" },
     from: { transform: "translateY(0px)" },
-    reset: true,
-    reverse: flip,
     config: config.gentle,
-    onRest: () => set(!flip),
   })
+
   return (
     <animated.span className={"arrow"} style={props}>
       <FontAwesomeIcon icon={faArrowDown} />
