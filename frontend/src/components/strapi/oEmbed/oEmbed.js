1c486221887559e018a7b314d44b585e8c3e2625
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-01-12 13:11:24 -0500

    Ajout de la possibilité d'avoir des urls de spotify.

--- frontend/src/components/strapi/oEmbed/oEmbed.js
+++ frontend/src/components/strapi/oEmbed/oEmbed.js
@@ -1,13 +1,30 @@
 import React from "react"
 import PropTypes from "prop-types"
 import ReactPlayer from "react-player"
+import "./oEmbed.scss"
 
 const OEmbed = ({ url }) => {
-  return (
-    <section className="container">
-      <ReactPlayer url={url} />
-    </section>
-  )
+  if (url.includes("spotify.com")) {
+    const info = url.split("/")
+    return (
+      <iframe
+        src={`https://open.spotify.com/embed/${info[info.length - 2]}/${
+          info[info.length - 1]
+        }`}
+        width="100%"
+        height="350"
+        frameBorder="0"
+        allow="encrypted-media"
+        title="Chanson"
+      />
+    )
+  } else {
+    return (
+      <section className="container">
+        <ReactPlayer url={url} />
+      </section>
+    )
+  }
 }
 
 OEmbed.propTypes = {
