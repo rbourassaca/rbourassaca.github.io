1bff12ba2d6317ad865080d6ed00276ea5718cca
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-04-26 17:07:04 -0400

    Mise en place de base de frontmatter.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	webpack: (cfg) => {
+		cfg.module.rules.push({
+			test: /\.md$/,
+			loader: 'frontmatter-markdown-loader',
+			options: {
+				mode: ['react-component'],
+			},
+		})
+		return cfg
+	},
 }
 
 module.exports = nextConfig
