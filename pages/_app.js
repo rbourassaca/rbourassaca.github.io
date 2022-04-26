c2bd35f04c852014491b7a6d6dff20b908c62909
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-04-26 14:55:45 -0400

    Ajout de code de redirection pour Netlify.

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,15 @@ import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+	if (window.netlifyIdentity) {
+		window.netlifyIdentity.on('init', (user) => {
+			if (!user) {
+				window.netlifyIdentity.on('login', () => {
+					document.location.href = '/admin/'
+				})
+			}
+		})
+	}
 	return (
 		<>
 			<Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
