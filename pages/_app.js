8831efc9f37b5185972ebf19369cf00bfdaefa0d
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-04-26 15:08:52 -0400

    Utilisation de useEffect.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 import Script from 'next/script'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-	if (window.netlifyIdentity) {
-		window.netlifyIdentity.on('init', (user) => {
-			if (!user) {
-				window.netlifyIdentity.on('login', () => {
-					document.location.href = '/admin/'
-				})
-			}
-		})
-	}
+	useEffect(() => {
+		if (window.netlifyIdentity) {
+			window.netlifyIdentity.on('init', (user) => {
+				if (!user) {
+					window.netlifyIdentity.on('login', () => {
+						document.location.href = '/admin/'
+					})
+				}
+			})
+		}
+	})
 	return (
 		<>
 			<Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
