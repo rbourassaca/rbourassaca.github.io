7f8de48a7397240e235da11b78d332af8e4d29fc
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-04-26 11:55:43 -0400

    Ajout de Netlify CMS.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
