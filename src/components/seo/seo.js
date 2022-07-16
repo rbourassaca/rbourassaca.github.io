1d511bcbd3970ab7eb0c4413dd84b4b6ab424693
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-07-16 16:29:31 -0400

    Amélioration du SEO.

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import { Helmet } from "react-helmet";
 
 const Seo = ({ title }) => {
-  return <Helmet title={title} />;
+  const {
+    directus: {
+      frontendSettings: { name },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      directus {
+        frontendSettings {
+          name
+        }
+      }
+    }
+  `);
+  return title ? (
+    <Helmet title={title + " | " + name} />
+  ) : (
+    <Helmet title={name} />
+  );
 };
 
 export default Seo;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,8 @@ import Seo from "../components/seo";
 const IndexPage = () => {
   return (
     <>
-      <Seo title={"Gatsby site!"} />
-      <main>
-        <h1>
-          Congratulations
-          <br />
-          <span>— you just made a Gatsby site! </span>
-          <span role="img" aria-label="Party popper emojis">
-            🎉🎉🎉
-          </span>
-        </h1>
-        <p>
-          Edit <code>src/pages/index.js</code> to see this page update in
-          real-time.{" "}
-          <span role="img" aria-label="Sunglasses smiley emoji">
-            😎
-          </span>
-        </p>
-      </main>
+      <Seo />
+      <Profile />
     </>
   );
 };
