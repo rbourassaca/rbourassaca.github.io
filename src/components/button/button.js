1b9506254c3c330636a631fdaafefd06058b8b4e
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-07-25 18:34:14 -0400

    Ajout du lancement d'une fonction passé en props et affichage de l'état toggle.

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,19 +1,27 @@
 import React from "react";
 import backgroundHighlightColors from "../../lib/backgroundHighlightColors";
 
-const Button = ({ highlightColor, text }) => {
+const Button = ({ highlightColor, text, action, toggle }) => {
   let colorClass = backgroundHighlightColors(highlightColor);
   return (
     <button
       className={
-        "group flex flex-row items-center space-x-1 bg-gray-100 dark:bg-gray-700 w-fit pt-1 pb-1 pl-4 pr-4 rounded-full transition-colors " +
-        colorClass.backgroundHighlightColorHover
+        "group flex flex-row items-center space-x-1 bg-gray-100 dark:bg-gray-700 w-fit pt-1 pb-1 pl-4 pr-4 rounded-full transition-colors" +
+        (toggle
+          ? " " +
+            colorClass.backgroundHighlightColor +
+            " " +
+            colorClass.backgroundHighlightColorDark
+          : " " + colorClass.backgroundHighlightColorHover)
       }
+      onClick={action}
     >
       <span
         className={
           "w-2 h-2 rounded-full group-hover:bg-gray-100 dark:group-hover:bg-gray-700 " +
-          colorClass.backgroundHighlightColor
+          (toggle
+            ? "bg-gray-100 dark:bg-gray-700"
+            : colorClass.backgroundHighlightColor)
         }
       />
       <p className={"capitalize"}>{text}</p>
--- src/lib/backgroundHighlightColors/backgroundHighlightColors.js
+++ src/lib/backgroundHighlightColors/backgroundHighlightColors.js
@@ -7,82 +7,105 @@
 
 const backgroundHighlightColors = (colorName) => {
   let backgroundHighlightColor;
+  let backgroundHighlightColorDark;
   let backgroundHighlightColorHover;
   switch (colorName) {
     case "#EF4444":
       backgroundHighlightColor = "bg-red-500";
+      backgroundHighlightColorDark = "dark:bg-red-500";
       backgroundHighlightColorHover = "hover:bg-red-500";
       break;
     case "#F97316":
       backgroundHighlightColor = "bg-orange-500";
+      backgroundHighlightColorDark = "dark:bg-orange-500";
       backgroundHighlightColorHover = "bg-orange-500";
       break;
     case "#F59E0B":
       backgroundHighlightColor = "bg-amber-500";
+      backgroundHighlightColorDark = "dark:bg-amber-500";
       backgroundHighlightColorHover = "hover:bg-amber-500";
       break;
     case "#EAB308":
       backgroundHighlightColor = "bg-yellow-500";
+      backgroundHighlightColorDark = "dark:bg-yellow-500";
       backgroundHighlightColorHover = "hover:bg-yellow-500";
       break;
     case "#84CC16":
       backgroundHighlightColor = "bg-lime-500";
+      backgroundHighlightColorDark = "dark:bg-lime-500";
       backgroundHighlightColorHover = "hover:bg-lime-500";
       break;
     case "#22C55E":
       backgroundHighlightColor = "bg-green-500";
+      backgroundHighlightColorDark = "dark:bg-green-500";
       backgroundHighlightColorHover = "hover:bg-green-500";
       break;
     case "#10B981":
       backgroundHighlightColor = "bg-emerald-500";
+      backgroundHighlightColorDark = "dark:bg-emerald-500";
       backgroundHighlightColorHover = "hover:bg-emerald-500";
       break;
     case "#14B8A6":
       backgroundHighlightColor = "bg-teal-500";
+      backgroundHighlightColorDark = "dark:bg-teal-500";
       backgroundHighlightColorHover = "hover:bg-teal-500";
       break;
     case "#06B6D4":
       backgroundHighlightColor = "bg-cyan-500";
+      backgroundHighlightColorDark = "dark:bg-cyan-500";
       backgroundHighlightColorHover = "hover:gb-cyan-500";
       break;
     case "#0EA5E9":
       backgroundHighlightColor = "bg-sky-500";
+      backgroundHighlightColorDark = "dark:bg-sky-500";
       backgroundHighlightColorHover = "hover:bg-sky-500";
       break;
     case "#3B82F6":
       backgroundHighlightColor = "bg-blue-500";
+      backgroundHighlightColorDark = "dark:bg-blue-500";
       backgroundHighlightColorHover = "hover:bg-blue-500";
       break;
     case "#6366F1":
       backgroundHighlightColor = "bg-indigo-500";
+      backgroundHighlightColorDark = "dark:bg-indigo-500";
       backgroundHighlightColorHover = "hover:gb-indigo-500";
       break;
     case "#8B5CF6":
       backgroundHighlightColor = "bg-violet-500";
+      backgroundHighlightColorDark = "dark:bg-violet-500";
       backgroundHighlightColorHover = "hover:bg-violet-500";
       break;
     case "#A855F7":
       backgroundHighlightColor = "bg-purple-500";
+      backgroundHighlightColorDark = "dark:bg-purple-500";
       backgroundHighlightColorHover = "hover:bg-purple-500";
       break;
     case "#C026D3":
       backgroundHighlightColor = "bg-fuchsia-500";
+      backgroundHighlightColorDark = "dark:bg-fuchsia-500";
       backgroundHighlightColorHover = "bg-fuchsia-500";
       break;
     case "#EC4899":
       backgroundHighlightColor = "bg-pink-500";
+      backgroundHighlightColorDark = "dark:bg-pink-500";
       backgroundHighlightColorHover = "hover:bg-pink-500";
       break;
     case "#F43F5E":
       backgroundHighlightColor = "bg-rose-500";
+      backgroundHighlightColorDark = "dark:bg-rose-500";
       backgroundHighlightColorHover = "hover:bg-rose-500";
       break;
     default:
       backgroundHighlightColor = "bg-blue-500";
+      backgroundHighlightColorDark = "dark:bg-blue-500";
       backgroundHighlightColorHover = "hover:bg-blue-500";
       break;
   }
-  return { backgroundHighlightColor, backgroundHighlightColorHover };
+  return {
+    backgroundHighlightColor,
+    backgroundHighlightColorDark,
+    backgroundHighlightColorHover,
+  };
 };
 
 export default backgroundHighlightColors;
