7ac72972e96983cc45877751893aa42104b8ab51
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-08-29 12:10:27 -0400

    Utilisation de Gatsby Head API.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,52 +1,51 @@
 require("dotenv").config();
 
 module.exports = {
-  siteMetadata: {
-    title: `Raphael Bourassa`,
-    siteUrl: `https://rbourassa.ca`,
-  },
-  plugins: [
-    "gatsby-plugin-postcss",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: process.env.GOOGLETRACKINGID,
-      },
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    {
-      resolve: "gatsby-plugin-sharp",
-      options: {
-        defaults: {
-          placeholder: "blurred",
-        },
-      },
-    },
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "@directus/gatsby-source-directus",
-      options: {
-        url: process.env.APIURL,
-        auth: {
-          token: process.env.APIKEY,
-        },
-      },
-    },
-  ],
+	siteMetadata: {
+		title: `Raphael Bourassa`,
+		siteUrl: `https://rbourassa.ca`,
+	},
+	plugins: [
+		"gatsby-plugin-postcss",
+		{
+			resolve: "gatsby-plugin-google-analytics",
+			options: {
+				trackingId: process.env.GOOGLETRACKINGID,
+			},
+		},
+		"gatsby-plugin-image",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/images/icon.png",
+			},
+		},
+		{
+			resolve: "gatsby-plugin-sharp",
+			options: {
+				defaults: {
+					placeholder: "blurred",
+				},
+			},
+		},
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "@directus/gatsby-source-directus",
+			options: {
+				url: process.env.APIURL,
+				auth: {
+					token: process.env.APIKEY,
+				},
+			},
+		},
+	],
 };
--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -1,26 +1,21 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
-import { Helmet } from "react-helmet";
 
-const Seo = ({ title }) => {
-  const {
-    directus: {
-      frontendSettings: { name },
-    },
-  } = useStaticQuery(graphql`
-    query {
-      directus {
-        frontendSettings {
-          name
-        }
-      }
-    }
-  `);
-  return title ? (
-    <Helmet title={title + " | " + name} />
-  ) : (
-    <Helmet title={name} />
-  );
+const Head = ({ title }) => {
+	const {
+		directus: {
+			frontendSettings: { name },
+		},
+	} = useStaticQuery(graphql`
+		query {
+			directus {
+				frontendSettings {
+					name
+				}
+			}
+		}
+	`);
+	return title ? <title>{title + " | " + name}</title> : <title>{name}</title>;
 };
 
-export default Seo;
+export default Head;
