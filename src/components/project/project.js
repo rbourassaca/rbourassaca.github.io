6718b1f3c1dfe38b9ed0c8ee5ca815b6c73a0a06
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-08-31 18:57:40 -0400

    Utilisation de forEach.

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -3,16 +3,15 @@ import ReactMarkdown from "react-markdown";
 import Panel from "../panel";
 
 const Project = ({ project }) => {
-  let highlightColor = [];
-  project.categories.map(({ categories_id }) => {
-    highlightColor.push(categories_id.color);
-    return null;
-  });
-  return (
-    <Panel title={project.title} highlightColors={highlightColor}>
-      <ReactMarkdown>{project.content}</ReactMarkdown>
-    </Panel>
-  );
+	let highlightColor = [];
+	project.categories.forEach(({ categories_id }) => {
+		highlightColor.push(categories_id.color);
+	});
+	return (
+		<Panel title={project.title} highlightColors={highlightColor}>
+			<ReactMarkdown>{project.content}</ReactMarkdown>
+		</Panel>
+	);
 };
 
 export default Project;
