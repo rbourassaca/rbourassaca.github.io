d43ad8698908e0eca46f8fea9cf103350b71486a
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-08-31 19:08:17 -0400

    Utilisation de placeholder.

--- src/components/profile/cover.js
+++ src/components/profile/cover.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const Cover = () => {
 	return (
-		<div className={"bg-white dark:bg-gray-800 h-40 lg:h-80 w-full"}></div>
+		<div
+			className={
+				"bg-white dark:bg-gray-800 h-40 lg:h-80 w-full bg-no-repeat bg-cover bg-center"
+			}
+			style={{
+				backgroundImage:
+					"url(https://source.unsplash.com/category/wallpaper/1200x800)",
+			}}
+		></div>
 	);
 };
 
