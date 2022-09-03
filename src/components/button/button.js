ca3c3a02e14efcc450d929be37c5a6a63487ea1d
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-09-03 18:20:56 -0400

    Simplification d'utilisation des couleurs du cms.

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,32 +1,23 @@
 import React from "react";
-import backgroundHighlightColors from "../../lib/backgroundHighlightColors";
 
 const Button = ({ highlightColor, text, action, toggle }) => {
-  let colorClass = backgroundHighlightColors(highlightColor);
-  return (
-    <button
-      className={
-        "group flex flex-row items-center space-x-1 bg-gray-100 dark:bg-gray-700 pt-1 pb-1 pl-4 pr-4 rounded-full transition active:opacity-80" +
-        (toggle
-          ? " " +
-            colorClass.backgroundHighlightColor +
-            " " +
-            colorClass.backgroundHighlightColorDark
-          : " " + colorClass.backgroundHighlightColorHover)
-      }
-      onClick={action}
-    >
-      <span
-        className={
-          "w-2 h-2 rounded-full group-hover:bg-gray-100 dark:group-hover:bg-gray-700 " +
-          (toggle
-            ? "bg-gray-100 dark:bg-gray-700"
-            : colorClass.backgroundHighlightColor)
-        }
-      />
-      <p className={"capitalize"}>{text}</p>
-    </button>
-  );
+	return (
+		<button
+			className={
+				"group flex flex-row items-center space-x-1 bg-gray-100 dark:bg-gray-700 pt-1 pb-1 pl-4 pr-4 rounded-full transition active:opacity-80"
+			}
+			style={toggle ? { backgroundColor: highlightColor } : null}
+			onClick={action}
+		>
+			<span
+				className={
+					"w-2 h-2 rounded-full group-hover:bg-gray-100 dark:group-hover:bg-gray-700 bg-gray-100 dark:bg-gray-700"
+				}
+				style={!toggle ? { backgroundColor: highlightColor } : null}
+			/>
+			<p className={"capitalize"}>{text}</p>
+		</button>
+	);
 };
 
 export default Button;
--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -1,40 +1,30 @@
 import React from "react";
-import backgroundHighlightColors from "../../lib/backgroundHighlightColors";
 
 const Panel = ({ children, className, title, highlightColors }) => {
-  if (typeof highlightColors !== "undefined") {
-    if (Array.isArray(highlightColors)) {
-    } else {
-      highlightColors = [highlightColors];
-    }
-  } else {
-    highlightColors = [];
-  }
-  return (
-    <div
-      className={
-        "bg-white border border-gray-100 dark:border-gray-900 dark:bg-gray-800 rounded shadow-lg flex flex-col space-y-3 p-8 h-fit " +
-        className
-      }
-    >
-      <h2 className={"text-xl font-bold flex flex-row items-center capitalize"}>
-        {highlightColors.map((color, i) => {
-          color = backgroundHighlightColors(color);
-          return (
-            <span
-              key={i}
-              className={
-                "w-2 h-2 rounded-full mr-2 " + color.backgroundHighlightColor
-              }
-            />
-          );
-        })}
+	return (
+		<div
+			className={
+				"bg-white border border-gray-100 dark:border-gray-900 dark:bg-gray-800 rounded shadow-lg flex flex-col space-y-3 p-8 h-fit " +
+				className
+			}
+		>
+			<h2 className={"text-xl font-bold flex flex-row items-center capitalize"}>
+				{typeof highlightColors !== "undefined" &&
+					highlightColors.map((color, i) => {
+						return (
+							<span
+								key={i}
+								className={"w-2 h-2 rounded-full mr-2"}
+								style={{ backgroundColor: color }}
+							/>
+						);
+					})}
 
-        {title}
-      </h2>
-      {children}
-    </div>
-  );
+				{title}
+			</h2>
+			{children}
+		</div>
+	);
 };
 
 export default Panel;
