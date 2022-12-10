06d4396211db8afcf4e410fc6640865a2c3949e5
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-12-10 16:22:45 -0500

    Création d'un projet @sveltejs/kit.

--- src/components/icons/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  Facebook,
-  Github,
-  Instagram,
-  Tiktok,
-  Youtube,
-  Email,
-  Default,
-} from "./icons";
--- src/components/panel/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "./panel";
-export { PanelContainer } from "./panelContailer";
