f669c9bb7bcf9109302a275a111baa5f0b61f65b
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-12-11 21:16:20 -0500

    Utilisation du tag style.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,14 @@
 
 <slot />
 
+<!--suppress CssUnknownTarget -->
 <style lang="scss">
-	@use "../styles/main.scss";
+	@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+
+	:global(body){
+		font-family: 'Montserrat', sans-serif;
+		margin: 2rem 4rem;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
 </style>
