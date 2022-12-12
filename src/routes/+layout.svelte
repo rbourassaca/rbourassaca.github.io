f87b583442bea5d81d2eb673fb8ae27a1cbba521
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-12-11 21:25:08 -0500

    Utilisation de prettier.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  import "normalize.css"
+	import 'normalize.css';
 </script>
 
 <slot />
 
 <!--suppress CssUnknownTarget -->
 <style lang="scss">
-	@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 
-	:global(body){
+	:global(body) {
 		font-family: 'Montserrat', sans-serif;
 		margin: 2rem 4rem;
 		-webkit-font-smoothing: antialiased;
