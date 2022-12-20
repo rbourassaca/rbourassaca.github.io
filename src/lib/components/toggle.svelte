c5658e37961e0a470a1e5baf23ba3c84d4eac1db
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-12-19 21:19:27 -0500

    Ajout d'un "toggle" de base.

---
+++ src/lib/components/toggle.svelte
@@ -0,0 +1,65 @@
+<label>
+	<input type="checkbox" />
+	<span />
+</label>
+
+<style lang="scss">
+	$padding: 4px;
+	$sizeX: 55px;
+	/*	$sizeY
+	- doit être la moitier ou plus petit que la moitier que $sizeX
+	- calc($sizeX / 2) + $padding pour un point rond
+	*/
+	$sizeY: calc($sizeX / 2) + $padding;
+	$transitionSpeed: 0.2s;
+	$colorOn: green;
+	$colorOff: gray;
+	$colorDot: white;
+
+	// Le toggle
+	label {
+		position: relative;
+		display: inline-block;
+		width: $sizeX;
+		height: $sizeY;
+		// La boite par défaut du navigateur
+		input {
+			opacity: 0;
+			width: 0;
+			height: 0;
+		}
+		// Le fond du toggle
+		span {
+			position: absolute;
+			cursor: pointer;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			background-color: $colorOff;
+			transition: $transitionSpeed;
+			border-radius: calc(#{$sizeX} / 2 - #{$padding});
+		}
+		// Le point du toggle
+		span:before {
+			content: '';
+			position: absolute;
+			height: calc(#{$sizeY} - #{$padding} * 2);
+			width: calc(#{$sizeX} / 2 - #{$padding});
+			left: $padding;
+			bottom: $padding;
+			background-color: $colorDot;
+			transition: $transitionSpeed;
+			border-radius: calc(#{$sizeX} / 2 - #{$padding});
+		}
+		// Le fond tu toggle quand la checkbox est activé
+		input:checked + span {
+			background-color: $colorOn;
+		}
+
+		// Le point du toggle quand la checkbox est désactivé
+		input:checked + span:before {
+			transform: translateX(calc(#{$sizeX} / 2 - #{$padding}));
+		}
+	}
+</style>
