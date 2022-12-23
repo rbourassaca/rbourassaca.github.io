984931c689b8c66c83d68a46f1a2c8f979124732
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-12-23 17:29:00 -0500

    Ajout de mdsvex.

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
 		adapter: adapter()
