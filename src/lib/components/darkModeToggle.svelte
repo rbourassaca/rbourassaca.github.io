0ff9f38fabde702da4be1cdf8092fb7e001aef6f
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-12-29 21:26:24 -0500

    Optimisation du code pour le mode sombre.

--- src/lib/components/darkModeToggle.svelte
+++ src/lib/components/darkModeToggle.svelte
@@ -1,29 +1,46 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { detectDarkMode, setDarkMode } from '$lib/functions/darkMode';
+	import { setDarkMode, watchDarkMode } from '$lib/functions/darkMode';
 	import Toggle from '$lib/components/toggle.svelte';
+	// Variable utilisé pour le toggle
 	let dark: boolean;
 	/*
-		Utilisation du cycle de vie onMount afin d'évité une erreur si 
-		le code est éxécuté sur le serveur.
+	Utilisation du cycle de vie onMount afin d'évité une erreur si 
+	le code est éxécuté sur le serveur.
 	*/
 	onMount(() => {
 		/*
-			Création d'un setter afin de changer la variable dark à partir 
-			d'un event listener.
+		Création d'un setter afin de changer la variable dark à partir 
+		d'un event listener.
 		*/
 		let darkModeSetter = (value: boolean) => {
 			dark = value;
 		};
 		/*
-			Assignation de la valeur initial a la variable dark par le biais
-			de la fonction detectDarkMode avec les paramêtre true et
-			darkModeSetter afin d'écouter si un changement se produit pendant
-			que l'utilisateur est sur le site web.
+		Assignation de la valeur initial a la variable dark.
 		*/
-		dark = detectDarkMode(true, darkModeSetter);
+		dark = window.matchMedia('(prefers-color-scheme: light)').matches;
+		/*
+		Utilisation de la fonction watchDarkMode afin d'écouter les changements par rapport
+		au mode sombre.
+		*/
+		watchDarkMode(darkModeSetter);
 	});
 </script>
+
+<svelte:head>
+	<script lang="ts">
+		/*
+			Fonction utilisé pour détecter si le mode sombre est désactivé.
+			Si le mode sombre est activé, on ajoute la classe dark au document.
+			La fonction est utilisé dans l'entête du document pour éviter d'avoir un flash
+			de la couleur non désiré.
+		*/
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.documentElement.classList.add('dark');
+		}
+	</script>
+</svelte:head>
 
 <span class="darkModeToggle">
 	<Toggle enabled={dark} action={setDarkMode} />
--- src/lib/functions/darkMode.ts
+++ src/lib/functions/darkMode.ts
@@ -5,7 +5,7 @@ type darkModeSetterFunction = (value: boolean) => void;
 	Fonction créant un event listener pour pouvoir réagir
 	quand le mode sombre est activé ou désactivé par l'hote.
 */
-const watchDarkMode = (darkModeSetter: darkModeSetterFunction) => {
+export const watchDarkMode = (darkModeSetter: darkModeSetterFunction) => {
 	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
 		if (e.matches) {
 			setDarkMode(false);
@@ -14,29 +14,6 @@ const watchDarkMode = (darkModeSetter: darkModeSetterFunction) => {
 		}
 		darkModeSetter(e.matches ? false : true);
 	});
-};
-
-/*
-	Fonction utilisé pour détecter si le mode sombre est désactivé.
-	Le paramètre watch est utilisé si on veut suivre les changement après
-	avoir fait la première détection.
-	Le paramètre darkModeSetter est la fonction du setter pour changer la
-	valeur de l'état du mode sombre.
-*/
-export const detectDarkMode = (
-	watch: boolean,
-	darkModeSetter?: darkModeSetterFunction
-): boolean => {
-	if (watch === true && darkModeSetter) {
-		watchDarkMode(darkModeSetter);
-	}
-	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		setDarkMode(false);
-		return false;
-	} else {
-		setDarkMode(true);
-		return true;
-	}
 };
 
 /*
