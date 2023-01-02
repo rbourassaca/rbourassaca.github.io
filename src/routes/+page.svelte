cc53b0b8b5795d3764700e407f9062a5d0e2410d
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-01-02 16:27:58 -0500

    Chargement et affichage des projets.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import Filter from '$lib/components/filter.svelte';
+	import Panel from '$lib/components/panel.svelte';
+	const projects = Object.entries(import.meta.glob('/src/content/projects/*.svx', { eager: true }));
 </script>
 
 <Filter />
+
+{#each projects as [path, module]}
+	<Panel>
+		<svelte:component this={module.default} />
+	</Panel>
+{/each}
