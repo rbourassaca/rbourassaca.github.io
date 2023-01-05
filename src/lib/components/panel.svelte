8daab7fdc0e738b2087358e71a02b478f8274dcb
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-01-05 16:32:23 -0500

    Ajustement de l'affichage de l'état de la copie du lien.

--- src/lib/components/panel.svelte
+++ src/lib/components/panel.svelte
@@ -2,6 +2,18 @@
 	export let post: boolean = false;
 	export let slug: string = '';
 	import Button from './button.svelte';
+
+	let shared = false;
+
+	let copyUrl = () => {
+		if (!shared) {
+			navigator.clipboard.writeText(window.location.origin + '/projets/' + slug);
+			shared = true;
+			window.setTimeout(() => {
+				shared = false;
+			}, 2000);
+		}
+	};
 </script>
 
 <article>
@@ -20,15 +32,24 @@
 			</a>
 			<Button
 				action={() => {
-					navigator.clipboard.writeText(window.location.origin + '/projets/' + slug);
+					copyUrl();
 				}}
 			>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-					<path
-						d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z"
-					/>
-				</svg>
-				Partager
+				{#if shared}
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+						<path
+							d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+						/>
+					</svg>
+					Le lien à été copier!
+				{:else}
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+						<path
+							d="M224 0c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224zM64 160c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64H64V224h64V160H64z"
+						/>
+					</svg>
+					Copier le lien
+				{/if}
 			</Button>
 		</div>
 	{/if}
