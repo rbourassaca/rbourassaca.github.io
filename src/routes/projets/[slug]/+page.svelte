b1b0f44d6edc2359662ec61160b2f58996d98a34
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-01-05 17:05:59 -0500

    Ajout d'une page pour afficher un seul projet.

---
+++ src/routes/projets/[slug]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	import Panel from '$lib/components/panel.svelte';
+	import Button from '$lib/components/button.svelte';
+</script>
+
+<a href="/">
+	<Button>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+			<path
+				d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+			/>
+		</svg>
+		Revenir au profile
+	</Button>
+</a>
+<Panel>
+	<h2>{data.slug}</h2>
+</Panel>
+
+<style lang="scss">
+	@use '../../../lib/styles/var.scss';
+
+	a {
+		display: flex;
+		width: fit-content;
+		margin-bottom: var.$spacingBetweenElementsSmall;
+	}
+</style>
---
+++ src/routes/projets/[slug]/+page.ts
@@ -0,0 +1,8 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load = ({ params }) => {
+	return {
+		slug: params.slug
+	};
+};
