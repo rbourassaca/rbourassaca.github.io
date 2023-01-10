3cc98a26395633867fd730f72a736eddcda54f1c
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-01-10 16:35:54 -0500

    Rendre le code plus simple à lire.

--- src/lib/functions/projects.ts
+++ src/lib/functions/projects.ts
@@ -1,7 +1,9 @@
 import { getSlugFromPath } from './getSlugFromPath';
 
 export const getProjects = () => {
-	return Object.entries(import.meta.glob('/src/content/projects/*.svx', { eager: true }));
+	const glob_import = import.meta.glob('/src/content/projects/*.svx', { eager: true });
+	const projects = Object.entries(glob_import);
+	return projects;
 };
 
 export const getProject = (slug: string) => {
