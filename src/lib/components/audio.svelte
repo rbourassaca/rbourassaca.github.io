073b314e6b083a12914ed71529ad8d9a12a26b51
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-01-26 14:21:32 -0500

    Ajout d'un composant pour lire les fichiers audio.

---
+++ src/lib/components/audio.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let src: string;
+</script>
+
+<audio controls controlsList="nodownload">
+	<source {src} type="audio/mpeg" />
+	Le navigateur ne prend pas en charge l'élément audio.
+</audio>
+
+<style lang="scss">
+	@use '../styles/var.scss';
+	audio {
+		margin: var.$spacingBetweenElementsSmall 0;
+		width: 100%;
+	}
+</style>
