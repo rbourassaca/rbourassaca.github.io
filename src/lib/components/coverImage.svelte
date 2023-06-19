c1984e0a722860d6ba936fb64e0b16a33109fb85
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-06-18 22:31:51 -0400

    Ajout d'un role à l'image.

--- src/lib/components/coverImage.svelte
+++ src/lib/components/coverImage.svelte
@@ -2,7 +2,7 @@
 	import src from '$lib/content/images/cristian-guanipa-DhnnfJnjb7I-unsplash.jpg';
 </script>
 
-<img {src} alt="Entête, créer par Cristian Guanipa sur Unsplash." width="1200" height="300" />
+<img {src} alt="Entête, créer par Cristian Guanipa sur Unsplash." role="presentation" width="1200" height="300" />
 
 <style lang="scss">
 	@use '../styles/var';
