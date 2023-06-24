09e95e57be16b15fa4208e615591796caccbafff
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-06-24 17:27:48 -0400

    Assignation de la variable avant l'utilisation.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,17 @@
 	import Panel from '$lib/components/panel/panel.svelte';
 	import PostHeader from '$lib/components/postHeader.svelte';
 	import Filter from '$lib/components/filter.svelte';
-	import { projects } from '../stores'
+	import { projects } from '../stores';
 
-	let projectList: projectType[];
-	projects.subscribe(value => {
+	let projectList: projectType[] = [];
+	projects.subscribe((value) => {
 		projectList = value;
-	})
+	});
 	let filteredProjects = projectList;
 </script>
 
 <section>
-	<Filter projectList={projectList} bind:filteredProjects />
+	<Filter {projectList} bind:filteredProjects />
 	{#if projectList.length > 0}
 		{#each filteredProjects as project}
 			<Panel post={true} slug={project.slug}>
