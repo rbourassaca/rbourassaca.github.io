0dc01b238efb246057712cb716d566360ae20d63
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-12-03 20:31:58 -0500

    Mise à jouer de la version.

--- src/lib/content/projects/portfolio.svelte
+++ src/lib/content/projects/portfolio.svelte
@@ -7,7 +7,7 @@
 		categories: [categories.web],
 		tags: [tags.html, tags.css, tags.js, tags.ts, tags.svelte],
 		dateCreated: new Date('2018-10-01'),
-		dateUpdated: new Date('2023-08-06')
+		dateUpdated: new Date('2023-12-03')
 	};
 </script>
 
