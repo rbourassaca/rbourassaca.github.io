3d2fdac1bd708095041663015d8300aaf171c66d
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-12-03 21:37:37 -0500

    Ajout du projet Architects - Animals (Instrumentale).

---
+++ src/lib/content/projects/architects-animals.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" context="module">
+	import type { projectMetadataType } from '$lib/types/project';
+	import { categories, tags } from './_projectMetadata';
+
+	import Audio from '$lib/components/audio.svelte';
+	import src from '$lib/content/audio/architects_-_animals.mp3';
+
+	export const metadata: projectMetadataType = {
+		title: 'Architects - Animals (instrumentale)',
+		categories: [categories.audio],
+		tags: [tags.reaper],
+		dateCreated: new Date('2023-07-30'),
+		dateUpdated: undefined
+	};
+</script>
+
+<p>
+	Enregistrement, édition et mixage d'une version instrumentale de la chanson Animals de Architects.
+</p>
+<Audio {src} />
