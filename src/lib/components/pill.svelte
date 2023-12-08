2dacce633eb1ab01eef14d99b118f0d6a36d544d
Raphael Bourassa <bourassa.raphael@outlook.com>
2023-12-07 20:51:32 -0500

    Ajout de la possibilité d'afficher une icone & utilisation de border pour la couleur.

--- src/lib/components/pill.svelte
+++ src/lib/components/pill.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
 	export let text: string;
-	export let color: string;
+	export let color: string = 'var(--color-border)';
+	export let icon: string | undefined = undefined;
 </script>
 
-<div style={`background-color: ${color}`}>
+<div style={`border-color: ${color}`}>
+	{#if icon !== undefined}
+		{@html icon}
+	{/if}
 	<span>{text}</span>
 </div>
 
 <style lang="scss">
 	@use '../styles/var.scss';
 	div {
+		display: flex;
+		gap: var.$spacingBetweenElementsSmall;
 		padding: var.$spacingBetweenElementsSmall;
 		border-radius: 1rem;
 		width: fit-content;
 		font-size: 0.75rem;
-	}
-	span {
-		color: #000;
+		border: 1px solid;
+		:global {
+			svg {
+				width: 1rem;
+			}
+		}
 	}
 </style>
