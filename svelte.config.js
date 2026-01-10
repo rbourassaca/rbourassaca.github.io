05eede3cfc4fc4d026928faa6640e616fd9b694b
Raphael Bourassa <bourassa.raphael@outlook.com>
2026-01-10 11:05:02 -0500

    Utilisation de @sveltejs/adapter-cloudflare.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,7 @@ const config = {
 
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [preprocess()],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		adapter: adapter()
